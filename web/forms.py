e6726791a11d3eb754b7b84fb5498185fba32d2d
Azhar <azhar.ks786@gmail.com>
2022-12-22 13:30:00 +0530

    second commit

--- web/forms.py
+++ web/forms.py
@@ -1,6 +1,17 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from api.models import Questions
+
+class QuestionForm(forms.ModelForm):
+    class Meta:
+        model=Questions
+        fields=["title","description","image"]
+        widgets={
+            "title":forms.TextInput(attrs={"class":"form-conotrol"}),
+            "description":forms.Textarea(attrs={"class":"form-conotrol","row":5}),
+            "image":forms.FileInput(attrs={"class":"form-select"})
+        }
 
 class UserRegistrationForm(UserCreationForm):
 
